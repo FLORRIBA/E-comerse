b201aabb56a657052e5d6f8f6bd37afe8305f9a9
FLORRIBA <143880221+FLORRIBA@users.noreply.github.com>
2023-11-27 18:08:45 -0300

    cambios finales

--- js/adminGuard.js
+++ js/adminGuard.js
@@ -1,8 +1,6 @@
-
-/*Para que lo redireccione a HOME si SABEel la direccion...  */
 const currentUser=JSON.parse(localStorage.getItem('currentUser'))
 
-if(!currentUser  || currentUser.role !== 'ADMIN_ROLE'){ //si no tengo un usuario(null) || que tengo un usuario pero la propiedad role de mi currentUser no es el ADMIN ROLE
+if(!currentUser  || currentUser.role !== 'ADMIN_ROLE'){ 
 window.location.href='index.html'
 
 }
--- js/login.js
+++ js/login.js
@@ -1,49 +1,41 @@
-const users = JSON.parse(localStorage.getItem("users")) || []; //sino encontras nada por lo menos generame un array vacio---users=array vacio
+const users = JSON.parse(localStorage.getItem("users")) || [];
 console.log(users);
 const loginForm = document.getElementById("login-form");
 
 loginForm.addEventListener("submit", (evt) => {
-  evt.preventDefault(); //prevenir el comportamiento por defecto sino se recarga la pg!
+  evt.preventDefault();
 
-  const email = evt.target.elements.email.value; //     = const email = loginForm.elements.email
-  const password = evt.target.elements.password.value; // = const password= loginForm.elements.password
+  const email = evt.target.elements.email.value;
+  const password = evt.target.elements.password.value;
   const user = users.find((usr) => {
-    //find recorre 1 x 1 los usuarios
     console.log(usr.email);
 
-    //el email ingresado coincide con el usuario que estoy recorriendo ?
     if (usr.email.toLowerCase() === email.toLowerCase()) {
       return true;
     }
-
-    // return usr.email===email
   });
 
-  // return false
-  //--cortamos el submit ya que no existe el correo y el password es distinto.
   if (!user || user.password !== password) {
     Swal.fire({
       title: "Login Incorrecto",
       text: "Datos ingresados incorrectos",
       icon: "ERROR",
-      timer: 3000,
+      timer: 2000,
     });
     return;
   }
 
-  // delete user.password (propiedad orientada a objetos) =>no se guarda la prop.password = user.password=undefined
   user.password = undefined;
 
-  localStorage.setItem("currentUser", JSON.stringify(user)); //voy a guardar con key-currentUser el usuario que encontre, el user es un objeto (necesitamos setearlo Json)
+  localStorage.setItem("currentUser", JSON.stringify(user));
 
   Swal.fire({
     title: "Login Correcto",
-    text: "Sera redireccionado en un momento", // lo redireccionamos al home una vez que se loguea correctamente
+    text: "Sera redireccionado en un momento",
     icon: "SUCCES",
     timer: 2000,
   });
   setTimeout(function () {
-    //setTimeout recibia una funcion (ejecuta)en 1500ml segundos
-    window.location.href = "../../index.html"; //cambiamos la localizacion de su posiscion
+    window.location.href = "../../index.html";
   }, 1500);
 });
